70f620692ff975c3ba27c5ecdceaf07380212c66
cidesgo@gmail.com <cidesgo@gmail.com>
2023-12-27 23:13:23 -0600

    Agregando booleanos

---
+++ modulo-primerosPasos/clase-boolean.js
@@ -0,0 +1,18 @@
+const isActive = true;
+const hasPermission = false;
+
+//* Conversion implicita
+const result = 5 > 3;
+// console.log(result);
+
+const names = "hola";
+// console.log(!!names); //! Identifica si hay algo / convertir un valor a su equivalente booleano
+
+const isHere = true;
+// console.log(!isHere);
+
+//* conversion explicita
+const value = 0;
+const otherValue = -24;
+const explicitBoolean = Boolean(value);
+console.log(explicitBoolean);
