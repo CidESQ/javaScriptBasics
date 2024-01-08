9a580304d3d67bea139d9582f2c313b2c2a8446a
cidesgo@gmail.com <cidesgo@gmail.com>
2024-01-07 23:39:51 -0600

    Terminando ejercicio de funciones

--- modulo-funciones-this/clase-practico-funciones.js
+++ modulo-funciones-this/clase-practico-funciones.js
@@ -9,16 +9,20 @@ function PowerfullGirl(name, color, superPower) {
         console.log(`\nHey! Hands down I'm ${this.name} \n 
 with my ${this.color} super suit \n
 and you should be worried because my power is ${this.superPower} \n
-Am I the leader: ${this.isLeader}\n`);
+${this.isLeader ? "Im the leader" : "Im not the leader"}
+`);
+    };
+
+    this.setLeadership = function () {
+        this.isLeader = true;
+        console.log(`${this.name} has became the leador of the Powerpuff team`);
     };
 }
 
 const blossom = new PowerfullGirl("Blossom", "Pink", "Ice breath");
 const buttercup = new PowerfullGirl("Buttercup", "Grenn", "Super Strenght");
 const bubbles = new PowerfullGirl("Bubbles", "Blue", "Flight");
-
-blossom.isLeader = true;
-
+blossom.setLeadership();
 blossom.displayInfo();
 buttercup.displayInfo();
 bubbles.displayInfo();
